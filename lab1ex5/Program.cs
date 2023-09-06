76bd0080115784ae87f6bc35bcb54a42fb3402d6
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 11:23:01 +0200

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare
+             */
+            int[] tabel = new int[2]  ;
+            for (int i = 0; i < 2; i++)
+            {
+                tabel[i]= int.Parse(Console.ReadLine());
+            }
+            Array.Sort(tabel);
+            string myString = String.Join(",", tabel);
+            Console.WriteLine(myString);
+
+
+
+
+
+
+        }
+    }
+}
